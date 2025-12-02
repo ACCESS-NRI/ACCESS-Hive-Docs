62ecddea95de27433602a9cb3f900e7e3fdcc89b
Jasmeen Kaur <u6871990@anu.edu.au>
2025-12-02 14:45:17 +1100

    :sparkles: (#1083): Added the terminal icon roughly after the search bar

--- docs/js/miscellaneous.js
+++ docs/js/miscellaneous.js
@@ -259,10 +259,11 @@ function toggleTerminalAnimations() {
         // const h1 = document.querySelector('h1');
         // console.log("hi")
         // h1.parentElement.insertBefore(terminalAnimationsSwitchContainer, h1);
-        const h1 = document.querySelector('.md-header');
         console.log("hi");
-        console.log(h1)
-        h1.appendChild(terminalAnimationsSwitchContainer, h1);
+        const searchbar = document.querySelector('.md-search');
+        console.log("hi");
+        console.log(searchbar)
+        searchbar.after(terminalAnimationsSwitchContainer);
     }
 }
 
